654bb4ca1e81732d4199cc68e6aabb0de1771bd8
Your Name <you@example.com>
2021-02-27 10:30:10 -0500

    submit

--- 1.1-loops_conditional.instructions.js
+++ 1.1-loops_conditional.instructions.js
@@ -7,4 +7,30 @@
     3. Inside of the callback function, write a conditional that checks if the number is even or odd. If the number is even, console.log 'the number is even'. If the number is odd, console.log 'the number is odd'. Your console should now look like the screenshot below.
 */
 
-//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+
+let numLine = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+for (let i = 0; i < numLine.length; i++){
+    console.log(numLine[i]);
+}
+
+//function callBackfunc(num){
+  //return num + 1;              //<=====This was my step 2 results. I commented it out so I could show what came up with.
+//}  
+//let num = numLine
+//console.log(callBackfunc);
+
+for (var i=0; i<=9; i++) {
+    if (i === 0) {
+            console.log('The number is Even:',numLine[i]);
+    }
+    else if (i % 2 === 0) {
+            console.log('The number is Even:',numLine[i]);   
+    }
+    else {
+        console.log("The number is Odd");
+}
+  
+}
+
--- 1.2-arrays-objects.instructions.js
+++ 1.2-arrays-objects.instructions.js
@@ -7,3 +7,25 @@
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+let favoriteMovie = {
+    nameOfMovie: 'Wizard of Oz',
+    runTime: '1h 52m',
+    genre: 'musical',
+    characters: [
+                {Dorothy: 'red slippers'},
+                {Scarecrow: 'Brains'},
+                {cowardlyLion: 'Courage'},
+                {tinMan: 'A heart'},
+                {Toto: 'pet'},
+                {Glinda: 'Nice Witch'},
+                {lollipopKids: 'Munchkins'},
+                   ]
+        
+
+    
+    
+}
+
+//console.log(object.keys(favoriteMovie.nameOfMovie.runTime.genre).toString());
+//console.log(favoriteMovie.nameOfMovie.runTime.genre.characters[0,1,2,3,4,5,6]);
